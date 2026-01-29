6b1f47c6a4cfd5bb2026a9471193b626dd50094b
soyacen <soyacen@outlook.com>
2026-01-29 11:47:20 +0800

    feat(strconvx): 添加时间解析功能支持多种Google Proto类型
    - 新增ParseTime函数用于解析带时区的时间字符串
    - 添加ParseWrapperTimestamp函数包装时间戳protobuf类型
    - 实现ParseWrapperDateTime函数处理日期时间protobuf类型
    - 添加ParseWrapperTime函数处理日期protobuf类型
    - 实现ParseWrapperTimeOfDay函数处理时间ofday protobuf类型
    - 添加ParseWrapperDayOfWeek函数转换为星期protobuf枚举
    - 实现ParseWrapperMonth函数转换为月份protobuf枚举
    - 引入google.golang.org/genproto依赖以支持Google Proto类型
    - 更新go.mod和go.sum文件包含新的依赖项

--- go.mod
+++ go.mod
@@ -19,6 +19,7 @@ require (
 	github.com/kr/pretty v0.3.1 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
+	google.golang.org/genproto v0.0.0-20260128011058-8636f8732409 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- strconvx/parse_duration.go
+++ strconvx/parse_duration.go
@@ -1,10 +1,136 @@
 package strconvx
 
 import (
+	"errors"
 	"time"
 
+	"github.com/elastic/go-elasticsearch/v8/typedapi/types/enums/month"
+	"google.golang.org/genproto/googleapis/type/date"
+	"google.golang.org/genproto/googleapis/type/datetime"
+	"google.golang.org/genproto/googleapis/type/dayofweek"
+	"google.golang.org/genproto/googleapis/type/month"
+	"google.golang.org/genproto/googleapis/type/timeofday"
 	"google.golang.org/protobuf/types/known/durationpb"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
+
+func ParseTime(s string, layout string, loc *time.Location) (time.Time, error) {
+	return time.ParseInLocation(layout, s, loc)
+}
+
+func ParseWrapperTimestamp(s string, layout string, loc *time.Location) (*timestamppb.Timestamp, error) {
+	v, err := ParseTime(s, layout, loc)
+	if err != nil {
+		return nil, err
+	}
+	return timestamppb.New(v), nil
+}
+
+func ParseWrapperDateTime(s string, layout string, loc *time.Location) (*datetime.DateTime, error) {
+	v, err := ParseTime(s, layout, loc)
+	if err != nil {
+		return nil, err
+	}
+	_, offset := v.Zone()
+	dt := &datetime.DateTime{
+		Year:    int32(v.Year()),
+		Month:   int32(v.Month()),
+		Day:     int32(v.Day()),
+		Hours:   int32(v.Hour()),
+		Minutes: int32(v.Minute()),
+		Seconds: int32(v.Second()),
+		Nanos:   int32(v.Nanosecond()),
+		TimeOffset: &datetime.DateTime_UtcOffset{
+			UtcOffset: durationpb.New(time.Duration(offset) * time.Second),
+		},
+	}
+	return dt, nil
+}
+
+func ParseWrapperTime(s string, layout string, loc *time.Location) (*date.Date, error) {
+	v, err := ParseTime(s, layout, loc)
+	if err != nil {
+		return nil, err
+	}
+	dt := &date.Date{
+		Year:  int32(v.Year()),
+		Month: int32(v.Month()),
+		Day:   int32(v.Day()),
+	}
+	return dt, nil
+}
+
+func ParseWrapperTimeOfDay(s string, layout string, loc *time.Location) (*timeofday.TimeOfDay, error) {
+	v, err := ParseTime(s, layout, loc)
+	if err != nil {
+		return nil, err
+	}
+	dt := &timeofday.TimeOfDay{
+		Hours:   int32(v.Hour()),
+		Minutes: int32(v.Minute()),
+		Seconds: int32(v.Second()),
+		Nanos:   int32(v.Nanosecond()),
+	}
+	return dt, nil
+}
+
+func ParseWrapperDayOfWeek(s string, layout string, loc *time.Location) (dayofweek.DayOfWeek, error) {
+	v, err := ParseTime(s, layout, loc)
+	if err != nil {
+		return dayofweek.DayOfWeek_DAY_OF_WEEK_UNSPECIFIED, err
+	}
+	switch v.Weekday() {
+	case time.Sunday:
+		return dayofweek.DayOfWeek_SUNDAY, nil
+	case time.Monday:
+		return dayofweek.DayOfWeek_MONDAY, nil
+	case time.Tuesday:
+		return dayofweek.DayOfWeek_TUESDAY, nil
+	case time.Wednesday:
+		return dayofweek.DayOfWeek_WEDNESDAY, nil
+	case time.Thursday:
+		return dayofweek.DayOfWeek_THURSDAY, nil
+	case time.Friday:
+		return dayofweek.DayOfWeek_FRIDAY, nil
+	case time.Saturday:
+		return dayofweek.DayOfWeek_SATURDAY, nil
+	}
+	return dayofweek.DayOfWeek_DAY_OF_WEEK_UNSPECIFIED, errors.New("invalid day of week")
+}
+
+func ParseWrapperMonth(s string, layout string, loc *time.Location) (month.Month, error) {
+	v, err := ParseTime(s, layout, loc)
+	if err != nil {
+		return month.Month_MONTH_UNSPECIFIED, err
+	}
+	switch v.Month() {
+	case time.January:
+		return month.Month_JANUARY
+	case time.February:
+		return month.Month_FEBRUARY
+	case time.March:
+		return month.Month_MARCH
+	case time.April:
+		return month.Month_APRIL
+	case time.May:
+		return month.Month_MAY
+	case time.June:
+		return month.Month_JUNE
+	case time.July:
+		return month.Month_JULY
+	case time.August:
+		return month.Month_AUGUST
+	case time.September:
+		return month.Month_SEPTEMBER
+	case time.October:
+		return month.Month_OCTOBER
+	case time.November:
+		return month.Month_NOVEMBER
+	case time.December:
+		return month.Month_DECEMBER
+	}
+	return month.Month_MONTH_UNSPECIFIED, errors.New("invalid month")
+}
 
 func ParseDuration(s string) (time.Duration, error) {
 	return time.ParseDuration(s)
