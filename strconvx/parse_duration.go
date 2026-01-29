ccd3091b0512085f57bdfe8427ade4c550c2f837
soyacen <soyacen@outlook.com>
2026-01-29 11:47:38 +0800

    refactor(strconvx): 移除未使用的导入包
    - 移除了未使用的 github.com/elastic/go-elasticsearch/v8/typedapi/types/enums/month 包引用

--- strconvx/parse_duration.go
+++ strconvx/parse_duration.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"time"
 
-	"github.com/elastic/go-elasticsearch/v8/typedapi/types/enums/month"
 	"google.golang.org/genproto/googleapis/type/date"
 	"google.golang.org/genproto/googleapis/type/datetime"
 	"google.golang.org/genproto/googleapis/type/dayofweek"
